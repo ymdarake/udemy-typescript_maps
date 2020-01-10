910545b1967aba22fdf5eece79b414aaf4e1783f
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-11 12:55:32 +1300

    [59]Google Maps Integration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hi there!');
+import { User } from './User';
+import { Company } from './Company';
+
+const user = new User();
+
+console.log(user);
+
+const compnay = new Company();
+console.log(compnay);
+
+console.log(google);
